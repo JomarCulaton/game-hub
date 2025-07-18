e5d93668e726925304cfaad66b1ec4843e694e7d
JomarC <124145169+JomarCulaton@users.noreply.github.com>
2025-07-18 09:42:54 +0800

    Refactor: Extract a query object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
-    import useData from "./useData";
+    import { GameQuery } from "../App";
+import useData from "./useData";
 import { Genre } from "./useGenre";
 
     export interface Platform {
@@ -15,11 +16,14 @@ import { Genre } from "./useGenre";
     metacritic: number;
     }
 
-const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => 
+const useGames = (
+   gameQuery: GameQuery
+
+)   => 
       useData<Game>('/games', {
-        params: { genres: selectedGenre?.id, 
-            platforms: selectedPlatform?.id 
+        params: { genres: gameQuery.genre?.id, 
+            platforms: gameQuery.platform?.id 
         }}, 
-      [selectedGenre?.id, selectedPlatform?.id]);
+      [gameQuery]);
 
 export default useGames;
